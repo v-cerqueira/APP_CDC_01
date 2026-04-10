5b4cab7db8a07f95ec9dbda0d6d063b57043ad83
Vinicius Cerqueira Silva <vcerqueiraads@gmail.com>
2026-04-10 19:59:36 -0300

    envio inicial do projeto

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+import estilos from './src/styleSheet/estilo';
 
-export default function App() {
+function App() {
+  let img = "https://cdn-icons-png.flaticon.com/512/919/919851.png";
+  let autor = "Vinicius Cerqueira Silva";
+  let data = "10/04/2026";
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={estilos.container}>
+
+      <View style={estilos.header}>
+        <Text style={estilos.titulo}>FUNDAMENTOS II</Text>
+
+        <View style={estilos.avatarBox}>
+          <Image
+            source={{ uri: img }}
+            style={estilos.avatar}
+          />
+        </View>
+
+        <Text style={estilos.subtitulo}>DSV MOBILE</Text>
+      </View>
+
+      <View style={estilos.contentBox}>
+        <Text style={estilos.contentText}>CONTEÚDO</Text>
+      </View>
+
+      <View style={estilos.spacer} />
+
+      <View style={estilos.footer}>
+        <Text style={estilos.footerText}>{autor}</Text>
+        <Text style={estilos.footerText}>{data}</Text>
+      </View>
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
